36e7d6df522963f38f4e4a0faf1c172c173fed21
Dennis Mutuma <dennocapi@gmail.com>
2022-09-23 12:34:27 +0300

    Loading app assets and constants

---
+++ constants/index.js
@@ -0,0 +1,20 @@
+import constants from "./constants";
+import theme, { COLORS, SIZES, FONTS, lightTheme, darkTheme } from "./theme";
+import images from "./images";
+import icons from "./icons";
+import dummyData from "./dummyData";
+import GOOGLE_API_KEY from "./maps"
+
+export {
+    constants,
+    theme,
+    COLORS,
+    SIZES,
+    FONTS,
+    images,
+    icons,
+    dummyData,
+    lightTheme,
+    darkTheme,
+    GOOGLE_API_KEY
+}
